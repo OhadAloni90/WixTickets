e85226e68ed85c325d69bdfba53ad1d7475eb8b9
MSI\Ohad <ohadfatuna@gmail.com>
2022-01-16 22:25:07 +0200

    Part 1. b - Ticket item improvements
    Hide and restore tickets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,54 @@
 import React from 'react';
 import './App.scss';
 import {createApiClient, Ticket} from './api';
-
 export type AppState = {
 	tickets?: Ticket[],
+  ticketsClone: Ticket[],
 	search: string;
-	
+	hiddenTickets: number;
 }
-
 const api = createApiClient();
 
 export class App extends React.PureComponent<{}, AppState> {
 
+  
 	
 	state: AppState = {
-		search: ''
+		search: '',
+    hiddenTickets: 0,
+    ticketsClone: []
 	}
 
 	searchDebounce: any = null;
 
 	async componentDidMount() {
+    const getTickets = await api.getTickets()
 		this.setState({
-			tickets: await api.getTickets()
+			tickets: getTickets,
+      ticketsClone: getTickets
 		});
 	}
+
+  onClickHandler = (id: string, tickets: Ticket[]) => {
+    
+    let ticketsLeft = this.state.hiddenTickets;
+    ticketsLeft+=1;
+    this.setState({hiddenTickets: ticketsLeft})
+    
+    let filtteredArray = [...tickets];
+    let returnedArray = filtteredArray.filter(ticket => 
+      ticket.id !== id)
+      this.setState({
+        tickets: returnedArray
+      })
+    }
+
+    onRestoreHandler = (tickets: Ticket[]) => 
+    { 
+      
+      this.setState({tickets: tickets});
+      this.setState({hiddenTickets: 0})
+    }
 
 	renderTickets = (tickets: Ticket[]) => {
 
@@ -35,7 +60,7 @@ export class App extends React.PureComponent<{}, AppState> {
 			{filteredTickets.map((ticket) => (<li key={ticket.id} className='ticket'>
 				<div className='title-block'>
 					<h5 className='title'>{ticket.title}</h5>
-					<h4 className='hide-btn'>Hide</h4>
+					<h4 className='hide-btn' onClick={() => this.onClickHandler(ticket.id, tickets)}>Hide</h4>
 				</div>
 				<p className='content'>{ticket.content}</p> {/*show tickets content */}
 
@@ -58,14 +83,13 @@ export class App extends React.PureComponent<{}, AppState> {
 	}
 
 	render() {	
-		const {tickets} = this.state;
-
+		const {tickets, hiddenTickets, ticketsClone} = this.state;
 		return (<main>
 			<h1>Tickets List</h1>
 			<header>
 				<input type="search" placeholder="Search..." onChange={(e) => this.onSearch(e.target.value)}/>
 			</header>
-			{tickets ? <div className='results'>Showing {tickets.length} results</div> : null }	
+			{tickets ? <div className='results'>Showing {tickets.length} results {hiddenTickets ? <i>({hiddenTickets} hidden tickets) <a onClick={() => this.onRestoreHandler(ticketsClone) }>restore</a></i>  : null}	</div> : null }
 			{tickets ? this.renderTickets(tickets) : <h2>Loading..</h2>}
 		</main>)
 	}
